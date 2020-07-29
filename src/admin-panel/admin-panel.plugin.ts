fd9338a3ed5bfb4ba739c49e34ae8a41a3b38c4f
SergeyMell <sergey.mell@agilie.com>
2020-07-30 00:55:24 +0300

    Created User Resource for Admin panel

--- src/admin-panel/admin-panel.plugin.ts
+++ src/admin-panel/admin-panel.plugin.ts
@@ -1,12 +1,19 @@
 import { INestApplication } from '@nestjs/common';
+import {Database, Resource} from 'admin-bro-typeorm';
 
 import AdminBro from 'admin-bro';
+
 import * as AdminBroExpress from 'admin-bro-expressjs';
+import UserResource from './resources/user.resource';
 
 export async function setupAdminPanel(app: INestApplication): Promise<void> {
 
+  AdminBro.registerAdapter({ Database, Resource });
+
   const adminBro = new AdminBro({
-    resources: [],
+    resources: [
+      UserResource,
+    ],
     rootPath: '/admin',
   });
 
